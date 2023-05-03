95e7fd439256e04fa01997ab226120ad0629ce85
Augusto Brandão <augustobrandao.99@outlook.com>
2023-05-03 15:36:45 -0300

    props e components

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,9 +1,35 @@
 import HelloWorld from './HelloWord';
+import UserProfile from './UserProfile';
+import ShoppingList from './ShoppingList';
 
 //Componente que irá conter toda a aplicação
 function App() {
+
+  const joao = {
+    id: 102,
+    name: 'João',
+    email: 'joao@gmail.com',
+    avatar: 'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png'
+  };
+
+  const amelia = {
+    id: 77,
+    name: 'Amélia',
+    email: 'amelia@gmail.com',
+    avatar: 'https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png'
+  };
+
   //Retornar o componente HelloWorld
-  return <HelloWorld />
+  return (
+    <main>
+      <HelloWorld />
+      
+      /* Dois Componentes utilizando as props */
+      <UserProfile user={ joao }/>
+      <UserProfile user={ amelia }/>
+      <ShoppingList />
+    </main>
+  );
 }
 
 export default App;
---
+++ react-study/src/ShoppingList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class ShoppingList extends Component {
+  render() {
+    const shoppingList = ['leite', 'arroz', 'feijão', 'banana', 'carne'];
+    const itens = shoppingList.map((item, index) => {
+      return <li key={index}>{item}</li>
+    })
+
+    return (
+      <div>
+        <h2>Lista de compras</h2>
+        <ul>
+          {itens}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default ShoppingList;
